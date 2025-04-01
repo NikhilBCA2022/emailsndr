ce77741d2a26a4173a5143e08c6775d8583972ad
Nikhil J <nikhil89041@gmail.com>
2025-04-01 19:32:33 +0530

    email sender

---
+++ mail.py
@@ -0,0 +1,36 @@
+import smtplib
+import os
+
+from mail.mime.multipart import MIMEMultipart
+from mail.mime.text import MIMEText
+
+
+def snd_mail(wrk_flow_name , repo_name , wrk_flow_run_id , workflow_run_url): 
+    sender_mail = os.getenv('SENDER_EMAIL')
+    sender_password = os.getenv('SENDER_PASSWORD')
+    receiver_mail = os.getenv('RECEIVER_EMAIL')
+
+    subject = f"Workflow {wrk_flow_name} failed in {repo_name}"
+    body = f"Workflow {wrk_flow_name} failed in {repo_name}. Please check the logs for more details.\n More details : \nrun_id: {wrk_flow_run_id} \nrepo_name: {repo_name} \nworkflow_name: {wrk_flow_name} \nworkflow_run_url: {workflow_run_url} "
+    
+
+
+
+    msg = MIMEMultipart()
+
+    msg['From'] = sender_mail
+    msg['To'] = receiver_mail
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_mail, sender_password)
+        server.send_message(msg)
+        server.quit()
+        print("Email sent successfully")
+    except Exception as e:
+        print(f"Failed to send email: {e}")
+
+snd_mail(os.getenv('WORKFLOW_NAME'), os.getenv('REPO_NAME'), os.getenv('WORKFLOW_RUN_ID'), os.getenv('WORKFLOW_RUN_URL'))
