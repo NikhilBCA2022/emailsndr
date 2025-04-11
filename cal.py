7fa56015a93fa4c620946bdd762190681dc5d407
Nikhil J <nikhil89041@gmail.com>
2025-04-11 17:43:02 +0530

    python code addded

--- cal.py
+++ cal.py
@@ -3,3 +3,15 @@ num2=input("Enter second number: ")
 
 sum=int(num1)+int(num2)
 print("The sum is: ",sum)
+
+diff=int(num1)-int(num2)
+print("The difference is: ",diff)
+
+prod=int(num1)*int(num2)
+print("The product is: ",prod)
+
+div=int(num1)/int(num2)
+print("The division is: ",div)
+
+mod=int(num1)%int(num2)
+print("The modulus is: ",mod)
